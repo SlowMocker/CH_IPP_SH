fa7ef9f5ee93558337eb7ba94067b4cb38eef0a6
Smicro <M_wuwenhao@163.com>
2017-03-13 14:46:23 +0800

    倒入 AFNetworking 和 Masonry 第三方库

---
+++ CH_IPP_SH_SDK/CH_IPP_SH_SDK/IPPInterface/CHBusinessCloudInterface+Device.h
@@ -0,0 +1,13 @@
+//
+//  CHBusinessCloudInterface+Device.h
+//  CH_IPP_SH_SDK
+//
+//  Created by Wu on 17/3/13.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import "CHBusinessCloudInterface.h"
+
+@interface CHBusinessCloudInterface (Device)
+
+@end
