92d03d051c71efcfa5b1b14dbdff56ad035daecb
Smicro <M_wuwenhao@163.com>
2017-03-13 14:03:32 +0800

    工程创建

---
+++ CH_IPP_SH_SDK/CH_IPP_SH_SDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CH_IPP_SH_SDK
+//
+//  Created by Wu on 17/3/13.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
