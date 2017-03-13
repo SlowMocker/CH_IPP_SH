391ad5c1f777d90cf6b19497c478457e04979afd
Smicro <M_wuwenhao@163.com>
2017-03-13 15:34:28 +0800

    1. 添加长虹用户中心 SDK 2. 添加用户中心接口单例

---
+++ CH_IPP_SH_SDK/CH_IPP_SH_SDK/UserCenterInterface/CHUserCloudManager.h
@@ -0,0 +1,15 @@
+//
+//  CHUserCloudManager.h
+//  CH_IPP_SH_SDK
+//
+//  Created by Wu on 17/3/13.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHUserCloudManager : NSObject
+
+CH_SINGLETON_H(UCManager)
+
+@end
---
+++ CH_IPP_SH_SDK/CH_IPP_SH_SDK/UserCenterInterface/CHUserCloudManager.m
@@ -0,0 +1,15 @@
+//
+//  CHUserCloudManager.m
+//  CH_IPP_SH_SDK
+//
+//  Created by Wu on 17/3/13.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import "CHUserCloudManager.h"
+
+@implementation CHUserCloudManager
+
+CH_SINGLETON_M(UCManager)
+
+@end
