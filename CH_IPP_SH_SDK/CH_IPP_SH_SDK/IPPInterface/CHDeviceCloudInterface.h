26c541f6547ff0ddc5462195f51c356974a086ad
Smicro <M_wuwenhao@163.com>
2017-03-13 15:15:04 +0800

    添加设备云数据请求单例

---
+++ CH_IPP_SH_SDK/CH_IPP_SH_SDK/IPPInterface/CHDeviceCloudInterface.h
@@ -0,0 +1,15 @@
+//
+//  CHDeviceCloudInterface.h
+//  CH_IPP_SH_SDK
+//
+//  Created by Wu on 17/3/13.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHDeviceCloudInterface : NSObject
+
+CH_SINGLETON_H(DCManager)
+
+@end
---
+++ CH_IPP_SH_SDK/CH_IPP_SH_SDK/IPPInterface/CHDeviceCloudInterface.m
@@ -0,0 +1,15 @@
+//
+//  CHDeviceCloudInterface.m
+//  CH_IPP_SH_SDK
+//
+//  Created by Wu on 17/3/13.
+//  Copyright © 2017年 Wu. All rights reserved.
+//
+
+#import "CHDeviceCloudInterface.h"
+
+@implementation CHDeviceCloudInterface
+
+CH_SINGLETON_M(DCManager)
+
+@end
